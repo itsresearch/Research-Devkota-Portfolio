3abdbcf01f406e53b74a771991b93a51e62c1e39
Research Devkota <researchofficial55@gmail.com>
2025-07-21 11:02:35 +0545

    Config file added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // this is correct for custom domain
+  base: '/', // this is correct for custom domain
   plugins: [react()],
 });
