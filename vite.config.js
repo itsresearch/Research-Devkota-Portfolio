4f478e6c6095e56fb12bb0491b4d8d2289639b69
Research Devkota <researchofficial55@gmail.com>
2025-11-23 16:57:34 +0545

    Revert "Config file added"
    This reverts commit 3abdbcf01f406e53b74a771991b93a51e62c1e39.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Research-Devkota-Portfolio/',  // your repo name
+  base: "/", // this is correct for custom domain
   plugins: [react()],
 });
