008cc271e0028aec1c5c114099c266c46ae51715
Research Devkota <researchofficial55@gmail.com>
2025-11-25 11:32:00 +0545

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/", // this is correct for custom domain
-  plugins: [react()],
-});
